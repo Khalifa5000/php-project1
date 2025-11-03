b978861fdfcde2505af5273781110a01f97e1a68
khalifa5000 <m.khalifa5000@gmail.com>
2025-11-03 20:15:04 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+echo "<h1> Welcome to My PHP Page </h1>";
+
+
+echo "hello world <br>";
+
+$userName = "Mohamed";
+
+echo "Hello, " . $userName . "! ";
+
+
+
+// Display an image
+$path = "img/";
+$skyImage = "saudai1.jpg";
+
+echo "<br><img src='" . $path . $skyImage . "' alt='Sky Image' style='width:300px;height:auto;'><br>";
+
+
+// String variable
+$s = "The sky is beautiful today.";
+echo " <br>  $s <br>";
+
+var_dump($s);
+echo "<br>";
+
+print_r($s);
+echo "<br>";
+
+// Integer variable
+$age = 25;  
+echo "Age: $age <br>";
+var_dump($age);
+echo "<br>";
+print_r($age);
+echo "<br>";
+
+// Float variable
+ $fruits = array("Apple", "Banana", "Orange");
+ echo "Fruits: $fruits[0], $fruits[1], $fruits[2] <br>";
+
+ if (in_array("Banana", $fruits)) {
+    echo "Banana is in the fruits array. <br>";
+} else {
+    echo "Banana is not in the fruits array. <br>";     
+}
+
+// Associative array
+$bebsita = array(
+    "name" => "Bebsi",
+    "type" => "Soda",
+    "flavor" => "Cola"
+);
+echo "Bebsi Details: Name : " . $bebsita["name"] .
+ ", Type : " . $bebsita["type"] .
+ ", Flavor : " . $bebsita["flavor"] . "<br>";
+
+if (in_array("Soda", $bebsita)) {
+    echo "this is not healty. <br>";
+} else {
+    echo "this is healthy. <br>";     
+}   
+
+
+
+
+
+
+
+// Display current date and time
+echo date(" <br> Y-m-d H:i:s");
+
+
+
+?>
