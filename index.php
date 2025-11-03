67131c6e58affa30b17404851da4bc648081e61c
khalifa5000 <m.khalifa5000@gmail.com>
2025-11-03 20:37:15 +0300

    Edit

--- index.php
+++ index.php
@@ -62,7 +62,56 @@ if (in_array("Soda", $bebsita)) {
     echo "this is healthy. <br>";     
 }   
 
+// Function to greet user
 
+function greet($fristName, $lastName) {
+    return "Hello, " . $fristName . $lastName .  "! Welcome to our website.<br>";
+}
+echo greet("Mohamed ", "Khalifa");
+
+
+//switch statement
+$day = date("D");
+switch ($day) {
+    case "Mon":
+        echo "Today is Monday. <br>";
+        break;
+    case "Tue":
+        echo "Today is Tuesday. <br>";
+        break;
+    case "Wed":
+        echo "Today is Wednesday. <br>";
+        break;
+    case "Thu":
+        echo "Today is Thursday. <br>";
+        break;
+    case "Fri":
+        echo "Today is Friday. <br>";
+        break;
+    case "Sat":
+        echo "Today is Saturday. <br>";
+        break;
+    case "Sun":
+        echo "Today is Sunday. <br>";
+        break;
+    default:
+        echo "Unknown day. <br>";
+        break;
+}
+
+
+// Loop through numbers 1 to 5
+for ($i = 1; $i <= 5; $i++) {
+    echo "Number: " . $i . "<br>";
+
+}
+
+
+//for each loop through an array
+$colors = array("Red", "Green", "Blue");
+foreach ($colors as $color) {
+    echo "Color: " . $color . "<br>";
+}   
 
 
 
@@ -71,6 +120,25 @@ if (in_array("Soda", $bebsita)) {
 // Display current date and time
 echo date(" <br> Y-m-d H:i:s");
 
+// include another PHP file
+
+
+include 'footer.php';
+
+
+// require another PHP file
+
+require 'footer.php';
+
+
+//session example
+session_start();
+$_SESSION["username"] = "Mohamed_Khalifa";
+echo "Session username is " . $_SESSION["username"] . "<br>";
+
+
 
 
 ?>
+
+
